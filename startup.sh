cbe3b3b92860f11c6c53f822569a992785f2e875
Of5sg <aksel.kla@gmail.com>
2025-03-24 21:56:50 +0000

    litt mer arbeid på containeren

--- startup.sh
+++ startup.sh
@@ -1,2 +1,16 @@
+
+docker pull postgres:latest
+
+docker run -d \
+    --name post_Data \
+    -e POSTGRES_USER=testbruker \
+    -e POSTGRES_PASSWORD=testpassord \
+    -e POSTGRES_DB=testdb \
+    -p 5432:5432 \
+    -v postgres_data:/var/lib/postgresql/data \
+    postgres:latest
+
+sleep 10
+
 curl -O https://raw.githubusercontent.com/pthom/northwind_psql/refs/heads/master/northwind.sql
 
