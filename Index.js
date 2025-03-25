cd36a7efbfd5206d27c5bb24370aad4cd348897b
Of5sg <aksel.kla@gmail.com>
2025-03-25 09:30:08 +0000

    mer

--- Index.js
+++ Index.js
@@ -9,3 +9,11 @@ const client =  new Client({
     port: 5432,
     database: "testdb"
 });
+
+await client.connect();
+
+const resultat = client.query('SELECT * FROM orders;');
+
+console.log(resultat);
+
+await client.end();
