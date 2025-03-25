1020256053a4345382217942dfb5b30f25bd9c85
Of5sg <aksel.kla@gmail.com>
2025-03-25 20:05:58 +0000

    pool

--- Index.js
+++ Index.js
@@ -2,11 +2,11 @@ import pg from "pg";
 
 const postgresIP = process.env.CONTAINER_IP;
 
-// console.log(postgresIP);
+// bruke pool istedet
 
-const { Client } = pg;
+const { Pool } = pg;
 
-const client =  new Client({
+const pool = new Pool({
     user: "testbruker",
     password: "testpassord",
     host: "172.17.0.2",
@@ -14,16 +14,26 @@ const client =  new Client({
     database: "testdb"
 });
 
-await client.connect();
+// const { Client } = pg;
 
-const query = "SELECT * FROM customers ORDER BY company_name LIMIT 10";
+// const client =  new Client({
+//     user: "testbruker",
+//     password: "testpassord",
+//     host: "172.17.0.2",
+//     port: 5432,
+//     database: "testdb"
+// });
 
-await client.query(query)
-    .then(res => {
-        console.log(res);
-    })
-    .catch(err => {
-        console.log(err);
-    });
+// await client.connect();
 
-await client.end();
+// const query = "SELECT * FROM customers ORDER BY company_name LIMIT 10";
+
+// await client.query(query)
+//     .then(res => {
+//         console.log(res);
+//     })
+//     .catch(err => {
+//         console.log(err);
+//     });
+
+// await client.end();
