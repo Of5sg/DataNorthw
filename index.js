b8ead6b55f13a0c8246ed404fb06155b5b087241
Of5sg <aksel.kla@gmail.com>
2025-03-29 16:29:17 +0000

    funker, sålangt

--- index.js
+++ index.js
@@ -2,3 +2,19 @@ import express from "express";
 import { PrismaClient } from "@prisma/client";
 
 
+const prisma = new PrismaClient({
+    log: ['info']
+});
+
+async function main(){
+    const shippers = await prisma.shippers.findMany({
+        where: {
+            shipper_id: {
+                not: 1234,
+            }
+        }
+    });
+    console.log(shippers);
+}
+
+main();
