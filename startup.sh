a396e5ff2da336781ae282f15a1e7de925d67c35
Of5sg <aksel.kla@gmail.com>
2025-03-29 20:33:01 +0000

    ting som må gjøres

--- startup.sh
+++ startup.sh
@@ -1,18 +1,18 @@
 
 # docker pull postgres:latest
 
-# docker run -d \
-#     --name post_Data \
-#     -e POSTGRES_USER=testbruker \
-#     -e POSTGRES_PASSWORD=testpassord \
-#     -e POSTGRES_DB=testdb \
-#     -p 5432:5432 \
-#     -v postgres_data:/var/lib/postgresql/data \
-#     postgres:latest
+docker run -d \
+    --name post_Data \
+    -e POSTGRES_USER=testbruker \
+    -e POSTGRES_PASSWORD=testpassord \
+    -e POSTGRES_DB=testdb \
+    -p 5432:5432 \
+    -v postgres_data:/var/lib/postgresql/data \
+    postgres:latest
 
-# sleep 10
+sleep 10
 
-# cat ./northwind.sql | docker exec -i post_Data psql -U bruker -d testdb
+cat ./northwind.sql | docker exec -i post_Data psql -U bruker -d testdb
 
 # docker exec -it post_Data psql testdb -U testbruker
 
