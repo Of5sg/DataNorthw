175075b54d8d5e3b9c373077c7d7d0f2e83eb669
Of5sg <aksel.kla@gmail.com>
2025-03-30 16:21:09 +0000

    flere endringer

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# DataNorthw
+# DataBase
 ----------------------------
+Dette er en DAtabase som tar Northwind eksempelet.
+
+>startes ved å åpne prosjektet som ett codespace i github, og skrive det følgende i konsollen.
+```bash
+bash startup.sh
+``` 
--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@ docker run -d \
     --name post_Data \
     -e POSTGRES_USER=testbruker \
     -e POSTGRES_PASSWORD=testpassord \
-    -e POSTGRES_DB=testdb \
+    -e POSTGRES_DB=NorthW \
     -p 5432:5432 \
     -v postgres_data:/var/lib/postgresql/data \
     postgres:latest
@@ -14,7 +14,7 @@ curl -O https://raw.githubusercontent.com/pthom/northwind_psql/refs/heads/master
 
 sleep 1
 
-cat ./northwind.sql | docker exec -i post_Data psql -U testbruker -d testdb
+cat ./northwind.sql | docker exec -i post_Data psql -U testbruker -d NorthW
 
 # docker exec -it post_Data psql testdb -U testbruker
 
