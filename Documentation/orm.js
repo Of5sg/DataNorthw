9d943f6d8b28148b4080c2af30a872ffe7e7ec9f
Of5sg <aksel.kla@gmail.com>
2025-03-31 19:38:20 +0000

    mellomrom

--- Documentation/orm.js
+++ Documentation/orm.js
@@ -26,20 +26,20 @@ export class Customer {
 
 export class Order {
     constructor(order_id, customer_id, employee_id, order_date, required_date, shipped_date, ship_via, freight, ship_name, ship_address, ship_city, ship_region, ship_postal_code, ship_country){
-    this.order_id = order_id || null;                  // smallint NOT NULL,
-    this.customer_id = customer_id || "?";             // character varying(5),
-    this.employee_id = employee_id || null;            // smallint,
-    this.order_date = order_date || null;              // date,
-    this.required_date = required_date || null;        // date,
-    this.shipped_date = shipped_date || null;          // date,
-    this.ship_via = ship_via || null;                  // smallint,
-    this.freight = freight || null;                    // real,
-    this.ship_name = ship_name || "?";                 // character varying(40),
-    this.ship_address = ship_address || "?";           // character varying(60),
-    this.ship_city = ship_city || "?";                 // character varying(15),
-    this.ship_region = ship_region || "?";             // character varying(15),
-    this.ship_postal_code = ship_postal_code || "?";   // character varying(10),
-    this.ship_country = ship_country || "?";           // character varying(15)
+        this.order_id = order_id || null;                  // smallint NOT NULL,
+        this.customer_id = customer_id || "?";             // character varying(5),
+        this.employee_id = employee_id || null;            // smallint,
+        this.order_date = order_date || null;              // date,
+        this.required_date = required_date || null;        // date,
+        this.shipped_date = shipped_date || null;          // date,
+        this.ship_via = ship_via || null;                  // smallint,
+        this.freight = freight || null;                    // real,
+        this.ship_name = ship_name || "?";                 // character varying(40),
+        this.ship_address = ship_address || "?";           // character varying(60),
+        this.ship_city = ship_city || "?";                 // character varying(15),
+        this.ship_region = ship_region || "?";             // character varying(15),
+        this.ship_postal_code = ship_postal_code || "?";   // character varying(10),
+        this.ship_country = ship_country || "?";           // character varying(15)
     };
 };
 
