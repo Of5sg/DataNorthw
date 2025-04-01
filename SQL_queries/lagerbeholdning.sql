a99c05e06b44290d6f5cdbc9b58d5734172e45a5
Of5sg <aksel.kla@gmail.com>
2025-04-01 08:55:28 +0000

    sql

--- SQL_queries/lagerbeholdning.sql
+++ SQL_queries/lagerbeholdning.sql
@@ -1,4 +1,5 @@
-SELECT units_in_stock, count(order_details.product_id)
+SELECT count(order_details.product_id) as solgt, units_in_stock, product_name
 FROM products
 JOIN order_details ON order_details.product_id = products.product_id
-GROUP BY order_details.product_id;
+GROUP BY order_details.product_id, units_in_stock, product_name
+Order BY solgt;
---
+++ SQL_queries/lagerbeholdning_svar.sql
@@ -0,0 +1,81 @@
+ solgt | units_in_stock |           product_name           
+-------+----------------+----------------------------------
+     5 |             29 | Mishi Kobe Niku
+     6 |             11 | Gravad lax
+     6 |             39 | Genen Shouyu
+     6 |             15 | Chocolade
+     8 |              4 | Louisiana Hot Spiced Okra
+     9 |             49 | Schoggi Schokolade
+    10 |             65 | Valkoinen suklaa
+    10 |              0 | Chef Anton's Gumbo Mix
+    10 |             52 | Laughing Lumberjack Lager
+    12 |            120 | Grandma's Boysenberry Spread
+    12 |             13 | Aniseed Syrup
+    13 |              4 | Longlife Tofu
+    13 |              6 | Northwoods Cranberry Sauce
+    14 |            104 | Gustaf's Knäckebröd
+    14 |              5 | Rogede sild
+    14 |             86 | Queso Manchego La Pastora
+    14 |            101 | Röd Kaviar
+    15 |              9 | Mascarpone Fabioli
+    16 |             40 | Sir Rodney's Marmalade
+    17 |             24 | Vegie-spread
+    18 |             62 | Escargots de Bourgogne
+    18 |             76 | NuNuCa Nuß-Nougat-Creme
+    19 |            111 | Sasquatch Ale
+    20 |             61 | Tunnbröd
+    20 |             53 | Chef Anton's Cajun Seasoning
+    21 |             36 | Zaanse koeken
+    21 |             10 | Maxilaku
+    22 |             35 | Tofu
+    23 |             36 | Ravioli Angelo
+    24 |            113 | Sirop d'érable
+    24 |             27 | Gula Malacca
+    24 |             17 | Côte de Blaye
+    27 |             95 | Spegesild
+    27 |             42 | Carnarvon Tigers
+    28 |             17 | Ipoh Coffee
+    29 |             38 | Filo Mix
+    29 |             15 | Uncle Bob's Organic Dried Pears
+    30 |              0 | Perth Pasties
+    30 |             22 | Wimmers gute Semmelknödel
+    30 |             69 | Chartreuse verte
+    30 |             26 | Singaporean Hokkien Fried Mee
+    31 |             26 | Gudbrandsdalsost
+    31 |            112 | Inlagd Sill
+    32 |             15 | Gumbär Gummibärchen
+    32 |            112 | Geitost
+    32 |              0 | Thüringer Rostbratwurst
+    32 |             76 | Louisiana Fiery Hot Pepper Sauce
+    32 |             10 | Nord-Ost Matjeshering
+    33 |             31 | Ikura
+    33 |             26 | Rössle Sauerkraut
+    33 |            115 | Pâté chinois
+    34 |              6 | Scottish Longbreads
+    36 |             20 | Steeleye Stout
+    36 |             21 | Tourtière
+    37 |              0 | Alice Mutton
+    37 |             25 | Teatime Chocolate Biscuits
+    38 |             22 | Queso Cabrales
+    38 |             39 | Chai
+    38 |             14 | Mozzarella di Giovanni
+    38 |             32 | Original Frankfurter grüne Soße
+    39 |              3 | Sir Rodney's Scones
+    39 |             20 | Manjimup Dried Apples
+    39 |             57 | Lakkalikööri
+    39 |             15 | Outback Lager
+    40 |             24 | Konbu
+    41 |            123 | Boston Crab Meat
+    42 |             26 | Flotemysost
+    43 |             29 | Pavlova
+    44 |             17 | Chang
+    46 |            125 | Rhönbräu Klosterbier
+    47 |             85 | Jack's New England Clam Chowder
+    48 |             17 | Tarte au sucre
+    50 |             21 | Gnocchi di nonna Alice
+    51 |             20 | Guaraná Fantástica
+    51 |             19 | Camembert Pierrot
+    51 |              0 | Gorgonzola Telino
+    54 |             79 | Raclette Courdavault
+(77 rows)
+
