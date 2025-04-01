46977c8243797ca012a386461e072a3b0ae7e07d
Of5sg <aksel.kla@gmail.com>
2025-04-01 19:32:03 +0000

    sql

--- SQL_queries/maned-salg/maned_salg.sql
+++ SQL_queries/maned-salg/maned_salg.sql
@@ -1,5 +1,11 @@
+
+-- her velger jeg en måned av gangen
+-- henter, og presenterer data for hver måned
+
 SELECT EXTRACT(MONTH FROM order_date) AS month, SUM(quantity) AS totalt
 FROM orders
 JOIN order_details ON order_details.order_id = orders.order_id
 GROUP BY month
 ORDER BY month;
+
+--- svaret virket ekstremt høyt
