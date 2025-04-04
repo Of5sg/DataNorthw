24d2773d1fb7cd7d572eb919abe2f4c101cc5da3
Of5sg <aksel.kla@gmail.com>
2025-04-04 09:11:59 +0000

    mer sql

--- SQL_queries/sesong/sesong_trend.sql
+++ SQL_queries/sesong/sesong_trend.sql
@@ -1,11 +1,22 @@
 SELECT 
     EXTRACT(YEAR FROM order_date) AS year,
-    EXTRACT(QUARTER FROM order_date) AS quarter, 
+    EXTRACT(QUARTER FROM order_date) AS quarter,
     ROUND(AVG(quantity)::numeric, 2) AS Avg_quantity,
     MAX(quantity) AS Max_quantity, MIN(quantity) AS Min_quantity
-    
-    -- EXTRACT(MONTH FROM order_date) AS month,
 FROM orders
 JOIN order_details ON orders.order_id = order_details.order_id
 GROUP BY year, quarter
 ORDER BY year, quarter;
+
+SELECT
+    EXTRACT(YEAR FROM order_date) AS year,
+    EXTRACT(MONTH FROM order_date) AS month,
+    ROUND(AVG(quantity)::numeric, 2) AS Avg_quantity,
+    MAX(quantity) AS Max_quantity, MIN(quantity) AS Min_quantity
+FROM orders
+JOIN order_details ON orders.order_id = order_details.order_id
+GROUP BY year, month
+ORDER BY year, month;
+
+
+-- bruke 2 separate queries og joine etter
