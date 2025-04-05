0a9948b745ed9f3743dee4baf1b8c58fa08facb2
Of5sg <aksel.kla@gmail.com>
2025-04-05 11:49:39 +0000

    noen endringer

--- SQL_queries/maned-salg/maned_salg.sql
+++ SQL_queries/maned-salg/maned_salg.sql
@@ -2,10 +2,13 @@
 -- her velger jeg en måned av gangen
 -- henter, og presenterer data for hver måned
 
-SELECT EXTRACT(MONTH FROM order_date) AS month, SUM(quantity) AS totalt
+SELECT 
+    EXTRACT(YEAR FROM order_date) AS year,
+    EXTRACT(MONTH FROM order_date) AS month, 
+    SUM(quantity) AS totalt
 FROM orders
 JOIN order_details ON order_details.order_id = orders.order_id
-GROUP BY month
-ORDER BY month;
+GROUP BY year, month
+ORDER BY year, month;
 
 --- svaret virket ekstremt høyt
