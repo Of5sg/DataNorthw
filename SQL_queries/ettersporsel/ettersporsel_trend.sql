1ddae42b59852f4a4408d651d3474baecf164ac7
Of5sg <aksel.kla@gmail.com>
2025-04-07 07:43:34 +0000

    sql

--- SQL_queries/ettersporsel/ettersporsel_trend.sql
+++ SQL_queries/ettersporsel/ettersporsel_trend.sql
@@ -7,7 +7,7 @@ FROM orders
 jOIN order_details ON orders.order_id = order_details.order_id
 JOIN products ON products.product_id = order_details.product_id
 -- GROUP BY month
-ORDER BY year, month, product_name;
+ORDER BY product_name, year, month;
 ------------------------------------------------------------
 -- SELECT order_date, product_name, quantity
 -- FROM order_details
