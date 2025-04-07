cfcf0d5569f7fc22325a2425140bd032e773d997
Of5sg <aksel.kla@gmail.com>
2025-04-07 06:51:18 +0000

    mer sql

--- SQL_queries/pris_vs_volum/pris_vs_volum.sql
+++ SQL_queries/pris_vs_volum/pris_vs_volum.sql
@@ -32,17 +32,35 @@
 
 -- fant en nettside som heter https://www.sql-practice.com/
 
---etter litt øving:
--- WITH A AS(
-    SELECT
-        product_id, 
-        EXTRACT(YEAR FROM order_date) AS year,
-        EXTRACT(MONTH FROM order_date) AS month,
-        ROUND((AVG(((unit_price * quantity) * ((100-discount)/100))/quantity))::numeric, 2) AS snitt_pris
-    FROM order_details
-    JOIN orders ON orders.order_id = order_details.order_id
-    GROUP BY year, month, product_id
-    ORDER BY product_id, year, month
--- )
--- SELECT * 
--- FROM A
+
+-- ----------------------
+-- SELECT
+--     product_id, 
+--     EXTRACT(YEAR FROM order_date) AS year,
+--     EXTRACT(MONTH FROM order_date) AS month,
+--     ROUND((AVG(((unit_price * quantity) * (100*discount))/quantity))::numeric, 2) AS snitt_pris
+-- FROM order_details
+-- JOIN orders ON orders.order_id = order_details.order_id
+-- GROUP BY year, month, product_id, discount
+-- ORDER BY product_id, year, month
+-- ----------------------
+
+
+
+SELECT 
+    EXTRACT(YEAR FROM order_date) AS year,
+    ROUND(AVG(((unit_price*quantity)*(1 - discount))/quantity)::numeric, 2) AS snitt_pris_per,
+    ROUND(AVG(discount)::numeric, 2) AS discount,
+    CASE
+        WHEN quantity < 20 THEN 'lavt volum' 
+        WHEN quantity > 50 THEN 'høyt volum'
+    ELSE
+        'middels volum'
+    END AS volum
+FROM order_details
+JOIN orders ON orders.order_id = order_details.order_id
+GROUP BY year, volum
+ORDER BY year
+
+
+--- det virker som man får høyere rabatt for større antall varer
