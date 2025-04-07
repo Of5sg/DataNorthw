0070fa2f6934180fd2f97aa7e0bdecb2709780ac
Of5sg <aksel.kla@gmail.com>
2025-04-07 20:14:03 +0000

    mer sql

--- SQL_queries/pris_vs_volum/pris_vs_volum.sql
+++ SQL_queries/pris_vs_volum/pris_vs_volum.sql
@@ -2,18 +2,18 @@
 
 SELECT 
     EXTRACT(YEAR FROM order_date) AS year,
+    EXTRACT(MONTH FROM order_date) AS month,
     ROUND(AVG(((unit_price*quantity)*(1 - discount))/quantity)::numeric, 2) AS snitt_pris_per,
     ROUND(AVG(discount)::numeric, 2) AS discount,
     CASE
-        WHEN quantity < 20 THEN 'lavt volum' 
-        WHEN quantity > 50 THEN 'høyt volum'
+        WHEN ROUND(AVG(quantity)::numeric, 2) < 20 THEN 'lavt volum' 
+        WHEN ROUND(AVG(quantity)::numeric, 2) > 25 THEN 'høyt volum'
     ELSE
         'middels volum'
     END AS volum
 FROM order_details
 JOIN orders ON orders.order_id = order_details.order_id
-GROUP BY year, volum
-ORDER BY year
+GROUP BY year, month
+ORDER BY year, month
 
-
---- det virker som man får høyere rabatt for større antall varer
+--- jeg ser ingen sammenheng mellom gjennomsnitt av antall varer og gjennomsnitt rabatt, på månedsbasis, har jeg gjort det feil?
--- SQL_queries/pris_vs_volum/pris_vs_volum_svar.sql
+++ SQL_queries/pris_vs_volum/pris_vs_volum_svar.sql
@@ -1,13 +1,27 @@
- year | snitt_pris_per | discount |     volum     
-------+----------------+----------+---------------
- 1996 |          17.14 |     0.10 | høyt volum
- 1996 |          20.70 |     0.04 | lavt volum
- 1996 |          23.84 |     0.06 | middels volum
- 1997 |          20.92 |     0.07 | høyt volum
- 1997 |          25.31 |     0.05 | lavt volum
- 1997 |          24.45 |     0.07 | middels volum
- 1998 |          33.61 |     0.08 | høyt volum
- 1998 |          26.99 |     0.04 | lavt volum
- 1998 |          25.37 |     0.06 | middels volum
-(9 rows)
+ year | month | snitt_pris_per | discount |     volum     
+------+-------+----------------+----------+---------------
+ 1996 |     7 |          19.37 |     0.06 | middels volum
+ 1996 |     8 |          20.77 |     0.04 | lavt volum
+ 1996 |     9 |          22.77 |     0.02 | lavt volum
+ 1996 |    10 |          20.45 |     0.07 | middels volum
+ 1996 |    11 |          26.21 |     0.04 | høyt volum
+ 1996 |    12 |          21.67 |     0.07 | høyt volum
+ 1997 |     1 |          22.76 |     0.07 | høyt volum
+ 1997 |     2 |          17.99 |     0.07 | høyt volum
+ 1997 |     3 |          20.92 |     0.05 | middels volum
+ 1997 |     4 |          26.63 |     0.05 | middels volum
+ 1997 |     5 |          26.66 |     0.05 | middels volum
+ 1997 |     6 |          24.13 |     0.06 | middels volum
+ 1997 |     7 |          25.18 |     0.06 | høyt volum
+ 1997 |     8 |          24.86 |     0.06 | middels volum
+ 1997 |     9 |          24.98 |     0.06 | middels volum
+ 1997 |    10 |          25.44 |     0.06 | høyt volum
+ 1997 |    11 |          24.09 |     0.05 | middels volum
+ 1997 |    12 |          28.84 |     0.07 | middels volum
+ 1998 |     1 |          30.48 |     0.06 | middels volum
+ 1998 |     2 |          26.47 |     0.04 | høyt volum
+ 1998 |     3 |          26.85 |     0.05 | middels volum
+ 1998 |     4 |          26.15 |     0.05 | høyt volum
+ 1998 |     5 |          19.36 |     0.06 | lavt volum
+(23 rows)
 
