0e586db6935e7dad2b9ae98d51f8e3f23045e23b
Of5sg <aksel.kla@gmail.com>
2025-05-26 07:56:05 +0000

    sql

--- SQL_queries/kategorier.sql
+++ SQL_queries/kategorier.sql
@@ -6,6 +6,7 @@ SELECT category_name, count(products.category_id) AS antall_salg_per_kategori
 FROM categories
 JOIN products ON categories.category_id = products.category_id
 JOIN order_details ON order_details.product_id = products.product_id
+WHERE (category_name LIKE $1 OR $1 = '')
 GROUP BY category_name
 ORDER BY antall_salg_per_kategori DESC;
 
